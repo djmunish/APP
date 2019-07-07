31cffcc611cf3cbe5375504313da3ccc3577bfc6
Mohit Dhiman <mohitd10@gmail.com>
2019-07-07 15:12:09 -0400

    Update_PlayArea

--- Battleship/src/PlayArea.java
+++ Battleship/src/PlayArea.java
@@ -2,18 +2,24 @@ import java.awt.GridLayout;
 import java.awt.*; 
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+//import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
 
 public class PlayArea extends JFrame{
-	
-	private final JSplitPane splitPane;  // split the window in top and bottom
-    private final JPanel topPanel;       // container panel for the top
-    private final JPanel bottomPanel;    // container panel for the bottom
-	private JScrollPane scrollPane;
+
+	private final JSplitPane splitPane1;
+	private final JSplitPane splitPane2;
+	private final JPanel topRight;       
+    private final JPanel topLeft;
+    private final JPanel bottomRight;
+    private final JPanel bottomLeft;    
+	private JScrollPane scrollPane1;
+	private JScrollPane scrollPane2;
+	private JScrollPane scrollPane3;
+	private JScrollPane scrollPane4;
 	
 	final JTable table1 = new JTable(new String[][]{
         {"*", "*", "*","*", "*", "*","*", "*", "*","*", "*"},
@@ -80,36 +86,52 @@ public class PlayArea extends JFrame{
 		
 		
 		JFrame f = new JFrame("PLAY AREA");
-		splitPane = new JSplitPane();
+		splitPane1 = new JSplitPane();
 		
-        topPanel = new JPanel();         // our top component
-        bottomPanel = new JPanel();      // our bottom component
-        
-        JScrollPane jp = new JScrollPane(table1); 
+		splitPane2 = new JSplitPane();
+
+		
+		topLeft = new JPanel();         
+        topRight = new JPanel();      
+        bottomLeft = new JPanel();
+        bottomRight = new JPanel();
+       
         setPreferredSize(new Dimension(800, 800)); 
             
         getContentPane().setLayout(new GridLayout()); 
                 
-        getContentPane().add(splitPane);
-        scrollPane = new JScrollPane(table1);
-        scrollPane = new JScrollPane(table2);
-//        scrollPane = new JScrollPane(table3);
-//        scrollPane = new JScrollPane(table4);
-//            
-            
-        JLabel jf = new JLabel("Hello");
-       
-        topPanel.add(jf);
-        bottomPanel.add(scrollPane); 
-        splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        getContentPane().add(splitPane1);
+        getContentPane().add(splitPane2);        
         
-        splitPane.setDividerLocation(400); 
-        splitPane.setTopComponent(topPanel);                  
-        splitPane.setBottomComponent(bottomPanel); 
-           
+        scrollPane1 = new JScrollPane(table1);
+        scrollPane2 = new JScrollPane(table2);
+        scrollPane3 = new JScrollPane(table3);
+        scrollPane4 = new JScrollPane(table4);
+       
+        topLeft.add(scrollPane1);
+        topRight.add(scrollPane2);
+        bottomLeft.add(scrollPane3);
+        bottomRight.add(scrollPane4); 
+       
+        splitPane1.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        splitPane2.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        
+        
+        
+        splitPane1.setDividerLocation(400);
+        splitPane2.setDividerLocation(400);
+        
+        
+        splitPane1.setTopComponent(topLeft);                  
+        splitPane1.setBottomComponent(bottomLeft); 
+        splitPane2.setTopComponent(topRight);        
+        splitPane2.setBottomComponent(bottomRight);        
+        
+        
+
         pack(); 
             
-            
+        
 	}
 }
             
