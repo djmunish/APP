6f71a974febd44e15c0af10c4e3a1799dde32164
Mohit Dhiman <mohitd10@gmail.com>
2019-07-07 23:07:30 -0400

    Arena_Updated

--- Battleship/src/Arena.java
+++ Battleship/src/Arena.java
@@ -1,10 +1,13 @@
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.Group;
@@ -15,14 +18,15 @@ import javafx.scene.paint.Color;
 
 public class Arena extends Application {
 
+    String selectedAddress;
     public void start(Stage stage) {
 
         try {
 
             // set title for the stage
-            stage.setTitle("Split Pane");
+            stage.setTitle("LET'S PLAY");
 
-            HBox hbox = new HBox(20);
+            HBox hbox = new HBox(70);
             hbox.setTranslateX(20);
             hbox.setTranslateY(20);
 
@@ -41,7 +45,7 @@ public class Arena extends Application {
 
             SplitPane split_pane1 = new SplitPane();
             split_pane1.setOrientation(Orientation.VERTICAL);
-            split_pane1.setPrefSize(400, 400);
+            split_pane1.setPrefSize(300, 40);
 
             split_pane1.getItems().addAll(createGrid(),createGrid());
 
@@ -50,22 +54,60 @@ public class Arena extends Application {
             // create split pane 2
 
             SplitPane split_pane2 = new SplitPane();
-            split_pane2.setPrefSize(400, 400);
+            split_pane2.setPrefSize(300, 400);
             split_pane2.setOrientation(Orientation.VERTICAL);
 
-            split_pane2.getItems().addAll(createGrid(), createGrid());
+
+
+
+            Player p1 = new Player();
+            p1.createInputs();
+
+            final ComboBox inputComboBox = new ComboBox();
+            inputComboBox.setStyle("-fx-border-color: #000000 ; -fx-border-width: 3px;");
+            inputComboBox.setStyle("-fx-border-color: #000000 ; -fx-background-color: #CD853F;");
+            inputComboBox.getItems().addAll(
+                    p1.inputs
+            );
+
+
+            inputComboBox.valueProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue ov, String t, String t1) {
+                    selectedAddress = t1;
+                }
+            });
+
+            Button btn = new Button();
+            btn.setText("Hit");
+            btn.setStyle("-fx-border-color: #000000 ; -fx-border-width: 3px;");
+            btn.setStyle("-fx-border-color: #000000; -fx-background-color: #CD853F");
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+
+                @Override
+                public void handle(ActionEvent event) {
+                    System.out.println("chako == " + selectedAddress);
+                }
+            });
+
+
+
+            split_pane2.getItems().addAll(createGrid(),createGrid());
+            hbox.getChildren().add(inputComboBox);
+            hbox.getChildren().add(btn);
+            hbox.setSpacing(50);
 
             hbox.getChildren().add(split_pane2);
 
-
             // Creating scene
 
-            Scene scene = new Scene(new Group(hbox), 400, 800);
-            scene.setFill(Color.GHOSTWHITE);
+            Scene scene = new Scene(new Group(hbox), 800, 800);
+            scene.setFill(Color.YELLOWGREEN);
             stage.setScene(scene);
-            stage.setTitle("SplitPane");
+
 
             stage.show();
+
         } catch (Exception e) {
 
             System.out.println(e.getMessage());
@@ -92,6 +134,8 @@ public class Arena extends Application {
                     String buttonname="button"+i+j;
                     Button button  = new Button(Integer.toString(i+1));
                     //button.setEnabled(false);
+                    button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                    button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                     button.setDisable(true);
                     gridPane.add(button, j, i);
                 }
@@ -102,60 +146,80 @@ public class Arena extends Application {
                         Button	button  = new Button("A");
                         //button.setEnabled(false);
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
                     }
                     else if(j==2){
                         Button	button  = new Button("B");
                         //button.setEnabled(false);
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
                     }
                     else if(j==3){
                         Button	button  = new Button("C");
                         //button.setEnabled(false);
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
                     }
                     else if(j==4){
                         Button	button  = new Button("D");
                         //button.setEnabled(false);
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
                     }
                     else if(j==5){
                         Button	button  = new Button("E");
                         //button.setEnabled(false);
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
                     }
                     else if(j==6){
                         Button	button  = new Button("F");
                         //button.setEnabled(false);
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
                     }
                     else if(j==7){
                         Button	button  = new Button("G");
                         //button.setEnabled(false);
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
                     }
                     else if(j==8){
                         Button	button  = new Button("H");
                         //button.setEnabled(false);
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
                     }
                     else if(j==9){
                         Button	button  = new Button("I");
                         //button.setEnabled(false);
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
                     }
                     else if(j==10){
                         Button	button  = new Button("J");
                         //button.setEnabled(false);
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
 
                     }
@@ -163,6 +227,8 @@ public class Arena extends Application {
                     else if(j==11){
                         Button	button  = new Button("K");
                         button.setDisable(true);
+                        button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                        button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFD700");
                         gridPane.add(button, j, i);
                     }
                 }
@@ -173,7 +239,10 @@ public class Arena extends Application {
                     nCols = j;
 
                     //ButtonClicks buttonsclk = new ButtonClicks(nRows,nCols);
-                    Button	button  = new Button();
+                    Button	button  = new Button("-");
+                    button.setStyle("-fx-border-color: #000000 ; -fx-border-width: 2px;");
+                    button.setStyle("-fx-border-color: #000000; -fx-background-color: #FFDAB9");
+                    button.setDisable(true);
 
                     gridPane.add(button, j, i);
 
