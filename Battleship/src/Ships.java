0370dc78d7efefec5dfd7f16d2c37fedd5fb12d5
djmunish <dev.munishsehdev@gmail.com>
2019-07-07 22:39:33 -0400

    ship changes

--- Battleship/src/Ships.java
+++ Battleship/src/Ships.java
@@ -23,11 +23,22 @@ public class Ships {
 		}//for	
 		return loc;
 	}
-	
+
 	public void setupShip(String start, String end) {
+
+
+
+
 		String[] xy1 = start.split("");
 		String[] xy2 = end.split("");
-		int var = xy1[0].compareTo(xy2[0]);
+
+
+		String x1 = Constants.indexToAlpha.get(xy1[0]);
+		String x2 = Constants.indexToAlpha.get(xy2[0]);
+
+
+
+		int var = x1.compareTo(x2);
 
 		char[] arr = Constants.alphabets.toCharArray();
 		if(var == 0) {
@@ -35,25 +46,25 @@ public class Ships {
 			
 			if(Integer.parseInt(xy1[1]) < Integer.parseInt(xy2[1])) {
 				for(int i = Integer.parseInt(xy1[1]) ; i<= Integer.parseInt(xy2[1]); i++) {
-					coordinates.add(xy1[0] + i);
+					coordinates.add(x1 + i);
 				}//for
 			}else {
 				for(int i = Integer.parseInt(xy2[1]) ; i>= Integer.parseInt(xy2[1]); i--) {
-					coordinates.add(xy1[0] + i);
+					coordinates.add(x1 + i);
 				}//for
 			}	
 		}//if equals
 		else if(var < 0){
-			int loci = findloc(xy1[0].charAt(0));
-			int locf = findloc(xy2[0].charAt(0));
+			int loci = findloc(x1.charAt(0));
+			int locf = findloc(x2.charAt(0));
 			for(int  i = loci ; i<= locf ; i++) {
 				String s = Character.toString(arr[i]) + xy1[1];
 				coordinates.add(s);
 			}//for
 		}else if(var > 0) {
 			
-			int loci = findloc(xy1[0].charAt(0));
-			int locf = findloc(xy2[0].charAt(0));
+			int loci = findloc(x1.charAt(0));
+			int locf = findloc(x2.charAt(0));
 			for(int  i = locf ; i>= loci ; i--) {
 				String s = Character.toString(arr[i]) + xy1[1];
 				coordinates.add(s);
