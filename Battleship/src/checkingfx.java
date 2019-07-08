0ce0628468dd49c3eb2095537d822ccfd9e3a295
Harsh Deep Kour <kourharsh@gmail.com>
2019-07-07 23:22:07 -0400

    checkingfx image updated

--- Battleship/src/checkingfx.java
+++ Battleship/src/checkingfx.java
@@ -1,5 +1,9 @@
 import java.awt.Insets;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+import javafx.scene.image.Image;  
+import javafx.scene.image.ImageView;  
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -17,12 +21,28 @@ public class checkingfx extends Application {
     }
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws FileNotFoundException {
         primaryStage.setTitle("Window to Choose Players");
+        FileInputStream input= new FileInputStream("battleship.jpg");
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image); 
+        imageView.setTranslateX(-220);
+        imageView.setTranslateY(10);
+        imageView.setFitHeight(450); 
+        imageView.setFitWidth(450); 
+        imageView.setPreserveRatio(true); 
         Button btn1 = new Button();
         Button btn2 = new Button();
-        btn1.setText("Play with Pc");
+        btn1.setText("Play with Computer");
         btn2.setText("Play with Another Player");
+        btn1.setStyle("-fx-background-color: Skyblue");
+        btn2.setStyle("-fx-background-color: Skyblue");
+        btn1.setTranslateX(400);
+        btn1.setTranslateY(350);
+        btn2.setTranslateX(145);
+        btn2.setTranslateY(450);
+        btn1.setPrefSize(250, 70);
+        btn2.setPrefSize(250, 70);
         btn1.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
@@ -45,9 +65,13 @@ public class checkingfx extends Application {
         
         FlowPane flow = new FlowPane();
         //flow.setPadding(new Insets(10, 10, 10, 10));
+        btn1.setLayoutX(350);
+        btn1.setLayoutY(350);
+        
         flow.setHgap(5);
-        flow.getChildren().addAll(btn1, btn2);
-        primaryStage.setScene(new Scene(flow, 300, 250));
+        flow.getChildren().addAll(btn1, btn2,imageView);
+        flow.setStyle("-fx-background-color: Grey");
+        primaryStage.setScene(new Scene(flow, 1000, 1000));
         primaryStage.show();
     }
 }
